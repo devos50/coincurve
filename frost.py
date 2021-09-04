f6d62c94d55ef94e085b8973f424ee197a943660
Martijn de Vos <mdmartijndevos@gmail.com>
2021-09-04 15:29:07 +0200

    Minor improvements

--- frost.py
+++ frost.py
@@ -5,13 +5,14 @@ from coincurve._libsecp256k1 import ffi, lib
 
 from coincurve import GLOBAL_CONTEXT
 
-NUM_PARTICIPANTS = 3
-THRESHOLD = 2
+NUM_PARTICIPANTS = 7
+THRESHOLD = 3
+ACTIVE_SIGNERS = 7
 context = GLOBAL_CONTEXT
 msg = os.urandom(32)
 session_id = os.urandom(32)
 participants = []
-active_participants = []
+active_participants = [ind + 1 for ind in range(ACTIVE_SIGNERS)]
 
 pubkeys = ffi.new("secp256k1_pubkey[%d]" % NUM_PARTICIPANTS)
 
@@ -80,9 +81,9 @@ class Participant:
         lib.secp256k1_scalar_set_b32(s, self.nonce.data, ffi.NULL)
         lib.secp256k1_ecmult_gen_with_ctx(context.ctx, rj, s)
         lib.secp256k1_ge_set_gej(rp, rj)
-        lib.secp256k1_pubkey_save(ffi.addressof(pubkeys, participant_index), rp)  # Save the nonced pubkey in pubkeys
+        lib.secp256k1_pubkey_save(ffi.addressof(pubkeys, self.session.my_index - 1), rp)  # Save the nonced pubkey in pubkeys
 
-    def compute_partial_signature(self, group_commitment, aggregated_pk, pk_parity):
+    def compute_partial_signature(self, group_commitment, aggregated_pk, active_participants, pk_parity):
         # Step 4+5 of the sign protocol
         scalar1 = ffi.new("secp256k1_scalar *")
         scalar2 = ffi.new("secp256k1_scalar *")
@@ -92,7 +93,7 @@ class Participant:
         # scalar2 is the group commitment
         lib.secp256k1_schnorrsig_challenge(c, group_commitment, msg, ffi.addressof(aggregated_pk, 1))  # Challenge stored in c
         lib.secp256k1_scalar_set_b32(scalar1, self.aggregated_share.data, ffi.NULL)
-        lib.secp256k1_frost_lagrange_coefficient(l, active_participants, THRESHOLD, self.session.my_index)
+        lib.secp256k1_frost_lagrange_coefficient(l, active_participants, len(active_participants), self.session.my_index)
         lib.secp256k1_scalar_mul(scalar1, scalar1, l)
         lib.secp256k1_scalar_mul(scalar2, c, scalar1)
         lib.secp256k1_nonce_function_frost(self.nonce, session_id, self.aggregated_share.data, msg,
@@ -106,13 +107,14 @@ class Participant:
 
 
 class Aggregator:
-    def __init__(self, participant):
+    def __init__(self, participant, n_signers):
         self.participant = participant
-        self.participant.session.n_signers = self.participant.threshold
+        self.participant.session.n_signers = n_signers
         self.signature = ffi.new("unsigned char[64]")
         self.aggregated_pk = ffi.new("unsigned char[33]")
+        self.group_commitment = ffi.new("unsigned char[33]")
 
-    def aggregate_shares(self, aggregated_shares):
+    def aggregate_shares(self, aggregated_shares, active_participants):
         scalar1 = ffi.new("secp256k1_scalar *")
         scalar2 = ffi.new("secp256k1_scalar *")
         rj = ffi.new("secp256k1_gej *")
@@ -122,7 +124,7 @@ class Aggregator:
 
         for participant_index, aggregated_share in aggregated_shares:
             l = ffi.new("secp256k1_scalar *")
-            lib.secp256k1_frost_lagrange_coefficient(l, active_participants, THRESHOLD, participant_index)
+            lib.secp256k1_frost_lagrange_coefficient(l, active_participants, len(active_participants), participant_index)
             lib.secp256k1_scalar_set_b32(scalar1, aggregated_share.data, ffi.NULL)
             lib.secp256k1_scalar_mul(scalar1, scalar1, l)
             lib.secp256k1_scalar_add(scalar2, scalar2, scalar1)
@@ -142,6 +144,14 @@ class Aggregator:
 
         ffi.memmove(ffi.addressof(self.signature, 0), group_commitment, 32)
         lib.secp256k1_scalar_get_b32(ffi.addressof(self.signature, 32), z)
+
+    def compute_group_commitment(self):
+        assert lib.secp256k1_frost_pubkey_combine(context.ctx, ffi.NULL, aggregator.participant.session,
+                                                  pubkeys)  # Override the combined_key in the session of the aggregator!
+        print("Combined key: %s" % hexlify(bytes(aggregator.participant.session.combined_pk.data)).decode())
+        assert lib.secp256k1_xonly_pubkey_serialize(context.ctx, self.group_commitment,
+                                                    ffi.addressof(aggregator.participant.session,
+                                                                  "combined_pk"))  # group_commitment <- serialized(combined_pk)
 
 
 # Initialize participants
@@ -178,38 +188,31 @@ for participant in participants:
 
     participant.aggregate_shares(rec_shares)
 
-# Set the participants that are going to sign
-for participant_index in range(THRESHOLD):
-    active_participants.append(participants[participant_index].session.my_index)
-
 # DONE BY AGGREGATOR
-aggregator = Aggregator(participants[0])
-aggregated_shares = [(participants[ind].session.my_index, participants[ind].aggregated_share) for ind in range(THRESHOLD)]
-aggregator.aggregate_shares(aggregated_shares)
+aggregator = Aggregator(participants[0], len(active_participants))
+aggregated_shares = [(ind, participants[ind - 1].aggregated_share) for ind in active_participants]
+aggregator.aggregate_shares(aggregated_shares, active_participants)
 
 # Generate nonce pks, done by participants
-for participant_index in range(THRESHOLD):
-    participant = participants[participant_index]
+for participant_index in active_participants:
+    participant = participants[participant_index - 1]
     participant.generate_nonced_pubkey()
-    print("Nonce pubkey of participant %d: %s" % (participant_index, hexlify(bytes(pubkeys[participant_index].data)).decode()))
+    print("Nonce pubkey of participant %d: %s" % (participant_index, hexlify(bytes(pubkeys[participant_index - 1].data)).decode()))
 
 # DONE BY AGGREGATOR
-group_commitment = ffi.new("unsigned char[33]")
-assert lib.secp256k1_frost_pubkey_combine(context.ctx, ffi.NULL, aggregator.participant.session, pubkeys)  # Override the combined_key in the session of the aggregator!
-print("Combined key: %s" % hexlify(bytes(aggregator.participant.session.combined_pk.data)).decode())
-assert lib.secp256k1_xonly_pubkey_serialize(context.ctx, group_commitment, ffi.addressof(aggregator.participant.session, "combined_pk"))  # group_commitment <- serialized(combined_pk)
+aggregator.compute_group_commitment()
 
 # TODO share group commitment with others
 
 # Generate partial signatures, done by participants
-for participant_index in range(THRESHOLD):
-    participant = participants[participant_index]
-    participant.compute_partial_signature(group_commitment, aggregator.aggregated_pk, aggregator.participant.session.pk_parity)
+for participant_index in active_participants:
+    participant = participants[participant_index - 1]
+    participant.compute_partial_signature(aggregator.group_commitment, aggregator.aggregated_pk, active_participants, aggregator.participant.session.pk_parity)
     print("Partial signature of participant %d: %s" % (participant_index, hexlify(bytes(participant.partial_signature)).decode()))
 
 # Combine partial signatures (step 7c)
-partial_signatures = [participants[ind].partial_signature for ind in range(THRESHOLD)]
-aggregator.compute_signature(partial_signatures, group_commitment)
+partial_signatures = [participants[ind - 1].partial_signature for ind in active_participants]
+aggregator.compute_signature(partial_signatures, aggregator.group_commitment)
 
 sig_hex = hexlify(bytes(aggregator.signature)).decode()
 print("Resulting Schnorr signature: %s" % sig_hex)
